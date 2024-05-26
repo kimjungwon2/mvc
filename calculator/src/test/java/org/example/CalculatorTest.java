0910dfc3a44ea3bff0ec8c9ce978340e28962db3
jungwon <mac2001035@gmail.com>
2024-05-26 18:44:45 +0900

    FEAT: 계산기 로직 테스트.

---
+++ calculator/src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CalculatorTest {
+
+    @DisplayName("덧셈 연산을 수행한다.")
+    @Test
+    void addtionTest(){
+        int result = Calculator.caluclate(1, "+", 2);
+
+        assertThat(result).isEqualsTo(3);
+
+    }
+
+}
