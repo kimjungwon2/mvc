c722ead04c15883faef5373f968d3b5839b3f85c
jungwon <mac2001035@gmail.com>
2024-05-27 17:36:30 +0900

    TEST: ParameterizedTest, MethodSource를 활용한 테스트

--- calculator/src/test/java/org/example/CalculatorTest.java
+++ calculator/src/test/java/org/example/CalculatorTest.java
@@ -2,8 +2,14 @@ package org.example;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class CalculatorTest {
 
@@ -16,4 +22,34 @@ class CalculatorTest {
 
     }
 
+    @DisplayName("뺄셈 연산을 수행한다.")
+    @Test
+    void subtractionTest(){
+        int result = Calculator.caluclate(1, "-", 2);
+
+        assertThat(result).isEqualTo(-1);
+
+    }
+
+    @DisplayName("덧셈 연산을 수행한다.")
+    @ParameterizedTest
+    @MethodSource("formulaAndResult")
+    void calculateTest(int operand1, String operator, int operand2, int result){
+      //given
+        int calculateResult = Calculator.caluclate(operand1,operator,operand2);
+
+      //when
+        assertThat(calculateResult).isEqualTo(result);
+
+      //then
+    }
+
+    private static Stream<Arguments> formulaAndResult(){
+        return Stream.of(
+                arguments(1,"+",2,3),
+                arguments(1,"-",2,-1),
+                arguments(4,"*",2,8),
+                arguments(4,"/",2,2)
+        );
+    }
 }
