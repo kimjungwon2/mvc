5ecfb12de2270be5f4625d1e7e03a033eae6716d
jungwon <mac2001035@gmail.com>
2024-05-28 02:35:06 +0900

    REFACOTR: ENUM으로 변환.

--- calculator/src/main/java/org/example/Calculator.java
+++ calculator/src/main/java/org/example/Calculator.java
@@ -2,17 +2,6 @@ package org.example;
 
 public class Calculator {
     public static int caluclate(int operand1, String operator, int operand2) {
-        if(operator.equals("+"))
-            return operand1 + operand2;
-        if(operator.equals("-"))
-            return operand1 - operand2;
-        if(operator.equals("*"))
-            return operand1 * operand2;
-        if(operator.equals("/"))
-            if(operand2 !=0)
-                return operand1 / operand2;
-            else
-                return 0;
-        return 0;
+        return ArithmeticOperator.calculate(operand1,operator,operand2);
     }
 }
