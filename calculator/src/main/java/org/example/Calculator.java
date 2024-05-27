45175e3f6ba93cb3cbc679028d79ec74cca1dddf
jungwon <mac2001035@gmail.com>
2024-05-27 17:27:41 +0900

    FEAT: 계산기 계산 리팩토링.

--- calculator/src/main/java/org/example/Calculator.java
+++ calculator/src/main/java/org/example/Calculator.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Calculator {
     public static int caluclate(int operand1, String operator, int operand2) {
-        return operand1 + operand2;
+        if(operator.equals("+"))
+            return operand1 + operand2;
+        return 0;
     }
 }
