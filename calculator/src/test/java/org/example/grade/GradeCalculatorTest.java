28291b503a2ec7f2f75b369ae416010ce1a8dd02
jungwon <mac2001035@gmail.com>
2024-05-31 01:12:45 +0900

    FEAT: OOP를 활용한 평균 학점 계산기.

--- calculator/src/test/java/org/example/grade/GradeCalculatorTest.java
+++ calculator/src/test/java/org/example/grade/GradeCalculatorTest.java
@@ -3,14 +3,25 @@ package org.example.grade;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 public class GradeCalculatorTest {
-    @DisplayName("")
+    @DisplayName("평균 학점을 계산한다.")
     @Test
-    void test(){
+    void calculateGradeTest(){
       //given
+        List<Course> courses = List.of(new Course("OOP", 3, "A+"),
+                                new Course("자료구조",3,"A+"));
+
+        GradeCalculator gradeCalculator = new GradeCalculator(courses);
 
       //when
+        double gradeResult = gradeCalculator.calculateGrade();
 
-      //then
+        //then
+        assertThat(gradeResult).isEqualTo(4.5);
     }
 }
