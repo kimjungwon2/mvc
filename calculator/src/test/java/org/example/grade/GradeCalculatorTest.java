103727aabe6adf8be3a072197cbca41c53e5313d
jungwon <mac2001035@gmail.com>
2024-05-31 00:53:35 +0900

    FEAT: 과목 생성.

---
+++ calculator/src/test/java/org/example/grade/GradeCalculatorTest.java
@@ -0,0 +1,16 @@
+package org.example.grade;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GradeCalculatorTest {
+    @DisplayName("")
+    @Test
+    void test(){
+      //given
+
+      //when
+
+      //then
+    }
+}
