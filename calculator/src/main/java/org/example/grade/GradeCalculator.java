274ffa8670e11718af4048e89cbe8519f5590829
jungwon <mac2001035@gmail.com>
2024-06-01 19:23:05 +0900

    REFACTOR: 책임 분리.

--- calculator/src/main/java/org/example/grade/GradeCalculator.java
+++ calculator/src/main/java/org/example/grade/GradeCalculator.java
@@ -4,24 +4,18 @@ import java.util.List;
 
 public class GradeCalculator {
 
-    private final List<Course> courses;
+    private final Courses courses;
 
     public GradeCalculator(List<Course> courses) {
-        this.courses = courses;
+        this.courses = new Courses(courses);
     }
 
     public double calculateGrade() {
-        double multipliedCreditAndCourseGrade =0;
-
-        for(Course course:courses){
-            multipliedCreditAndCourseGrade += course.multiplyCreditAndCourseGrade();
-        }
+        double totalMultipliedCreditAndCourseGrade = courses.multiplyCreditAndCourseGrade();
 
         //수강신청 총학점 수
-        int totalCompletedCredit = courses.stream()
-                .mapToInt(Course::getCredit)
-                .sum();
+        int totalCompletedCredit = courses.calculateTotalCompletedCredit();
 
-        return multipliedCreditAndCourseGrade /totalCompletedCredit;
+        return totalMultipliedCreditAndCourseGrade /totalCompletedCredit;
     }
 }
