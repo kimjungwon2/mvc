b0cc8eb9aa0c13e8d2fc3723a36220f1e57ed133
jungwon <mac2001035@gmail.com>
2024-06-02 14:37:27 +0900

    FEAT: 요리사 객체 생성. (TDD)

--- calculator/src/main/java/org/example/delivery/Cooking.java
+++ calculator/src/main/java/org/example/delivery/Cooking.java
@@ -2,7 +2,7 @@ package org.example.delivery;
 
 public class Cooking {
     public Cook makeCook(MenuItem menuItem) {
-        return new Cook("돈까스",5000);
+        return new Cook(menuItem);
     }
 
 }
