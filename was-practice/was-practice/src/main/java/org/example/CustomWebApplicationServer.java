0313fd1e741d52170798509e6e859e172c76cce3
jungwon <mac2001035@gmail.com>
2024-06-27 02:21:52 +0900

    FEAT: HTTP 구현.

--- was-practice/was-practice/src/main/java/org/example/CustomWebApplicationServer.java
+++ was-practice/was-practice/src/main/java/org/example/CustomWebApplicationServer.java
@@ -3,18 +3,15 @@ package org.example;
 
 import lombok.Getter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
 
 @Getter
 public class CustomWebApplicationServer {
     private final int port;
-
-    private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
     public CustomWebApplicationServer(int port) {
         this.port = port;
@@ -22,12 +19,26 @@ public class CustomWebApplicationServer {
 
     public void start(){
         try(ServerSocket serverSocket = new ServerSocket(port)){
-            logger.info("[CustomWebApplicationServer] started {} port.", port);
+            System.out.println(port);
             Socket clientSocket;
-            logger.info("[CustomWebApplicationServer] waiting for client.");
+            System.out.println("Waiting client");
 
             while((clientSocket = serverSocket.accept()) !=null){
-                logger.info("[CustomWebApplicationServer] client connected!");
+                System.out.println("[CustomWebApplicationServer] client connected!");
+
+                /**
+                 * 사용자 요청을 메인 Thread가 처리
+                 * **/
+
+                try(InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()){
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+                    DataOutputStream dos = new DataOutputStream(out);
+
+                    String line;
+                    while ((line = br.readLine()) != "") {
+                        System.out.println(line);
+                    }
+                }
 
             }
 
