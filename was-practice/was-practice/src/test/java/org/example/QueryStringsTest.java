848d24e10a86a17cd2d6b929a2b0266d563a4dd8
jungwon <mac2001035@gmail.com>
2024-07-01 18:32:44 +0900

    FEAT: Query Strings 만들기.

---
+++ was-practice/was-practice/src/test/java/org/example/QueryStringsTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class QueryStringsTest {
+    @DisplayName("")
+    @Test
+    void test(){
+      //given
+        QueryStrings queryStrings = new QueryStrings("operand1=11&operator=*&operand2=55"); //List<QueryString>
+        assertThat(queryStrings).isNotNull();
+    }
+}
