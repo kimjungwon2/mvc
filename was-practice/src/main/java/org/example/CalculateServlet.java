48f5c846e9d8b8f755b48f3a654d82514944e008
jungwon <mac2001035@gmail.com>
2024-07-09 00:55:52 +0900

    FEAT: Servlet Init, Service.

--- was-practice/src/main/java/org/example/CalculateServlet.java
+++ was-practice/src/main/java/org/example/CalculateServlet.java
@@ -1,15 +1,20 @@
 package org.example;
 
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
+@Slf4j
+@WebServlet("/calculate")
 public class CalculateServlet implements Servlet {
 
 
     @Override
     public void init(ServletConfig config) throws ServletException {
-
+        log.info("init");
     }
 
     @Override
@@ -19,7 +24,7 @@ public class CalculateServlet implements Servlet {
 
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-
+        log.info("service");
     }
 
     @Override
