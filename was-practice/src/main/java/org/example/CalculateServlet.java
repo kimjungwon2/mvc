c2f470b7cfb586dcbaf3c618ac6b90e8a24a931a
jungwon <mac2001035@gmail.com>
2024-07-09 00:59:29 +0900

    FEAT: 서블릿을 사용한 계산.

--- was-practice/src/main/java/org/example/CalculateServlet.java
+++ was-practice/src/main/java/org/example/CalculateServlet.java
@@ -2,10 +2,13 @@ package org.example;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.example.calculate.Calculator;
+import org.example.calculate.PositiveNumber;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Slf4j
 @WebServlet("/calculate")
@@ -23,8 +26,16 @@ public class CalculateServlet implements Servlet {
     }
 
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
         log.info("service");
+        int operand1 = Integer.parseInt(request.getParameter("operand1"));
+        String operator = request.getParameter("operator");
+        int operand2 = Integer.parseInt(request.getParameter("operand2"));
+
+        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+
+        PrintWriter writer = response.getWriter();
+        writer.println(result);
     }
 
     @Override
